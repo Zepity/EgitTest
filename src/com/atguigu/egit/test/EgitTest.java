179303333e5b998a96a96363423be7b4ca10db88
Zepity <1781799061@qq.com>
2019-09-19 11:47:34 +0800

    two

--- src/com/atguigu/egit/test/EgitTest.java
+++ src/com/atguigu/egit/test/EgitTest.java
@@ -2,6 +2,7 @@ package com.atguigu.egit.test;
 
 public class EgitTest {
 	public static void main(String[] args) {
-		System.out.println("aaa");
+		System.out.println("aaa"); 
+		System.out.println("bbb"); 
 	}
 }
